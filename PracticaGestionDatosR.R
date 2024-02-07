027950bca522a940055254a00dd47e902eb9721b
AleNValumno <158163590+AleNValumno@users.noreply.github.com>
2024-02-07 12:46:42 +0100

    lilmnhi

---
+++ PracticaGestionDatosR.R
@@ -0,0 +1,45 @@
+nuevo_dir<- "C:/practicaGestionDatosR"
+setwd(nuevo_dir)
+
+vector <- c("edades_descubrimientos")
+# 1
+edades_descubrimientos = c( 23, 35, 63, 24, 48)
+mean(edades_descubrimientos)
+
+# 2
+
+cantidad_artefactos = c( 53, 75, 29, 42, 24)
+sum(cantidad_artefactos)
+
+# 3
+
+profundidad_hallazgos = c(94, 29, 74, 29, 29)
+max(profundidad_hallazgos)
+
+# 4
+
+materiales_encontrados = c("huesos", "ceramica", "madera", "hierro", "calcica")
+length(unique(materiales_encontrados))
+
+# 5
+
+años_excavaciones = c("2001", "2012", "1977", "2004")
+length(unique(años_excavaciones))
+
+# 6
+
+matrix(c(5. 7, 3),
+       git.ini
+      
+
+
+
+# 7
+
+
+
+
+
+# 8
+
+
